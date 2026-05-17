5b913810b122e16ee6a1d943f547bed5e6eb9a5f
Haseeba-Imtiaz <haseebaimtiaz.design@gmail.com>
2026-05-17 10:51:57 +0100

    fix changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Men from './pages/Men';
+import Women from './pages/Women';
 
 function App() {
   return (
@@ -15,10 +17,10 @@ function App() {
       {/* Pages badalne ke raste */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
         
-        {/* Naye Pages jo aap baad mein banayengi, abhi ke liye Coming Soon dummy text lagaya hai */}
-        <Route path="/men" element={<div style={{ color: 'white', padding: '100px', textAlign: 'center' }}><h2>MEN'S COLLECTION</h2><p>Coming Soon...</p></div>} />
-        <Route path="/women" element={<div style={{ color: 'white', padding: '100px', textAlign: 'center' }}><h2>WOMEN'S COLLECTION</h2><p>Coming Soon...</p></div>} />
+        {/* Baki Pages ke dummy routes */}
         <Route path="/accessories" element={<div style={{ color: 'white', padding: '100px', textAlign: 'center' }}><h2>ACCESSORIES</h2><p>Coming Soon...</p></div>} />
         <Route path="/fitness" element={<div style={{ color: 'white', padding: '100px', textAlign: 'center' }}><h2>FITNESS GEAR</h2><p>Coming Soon...</p></div>} />
         <Route path="/sports-brand" element={<div style={{ color: 'white', padding: '100px', textAlign: 'center' }}><h2>SPORTS BRAND</h2><p>Coming Soon...</p></div>} />
